f800f148a2ecfc37b922a7a6153fa5b7f122e58c
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-15 11:14:06 +0530

    add: superglobals.

---
+++ 03superglobals/index.php
@@ -0,0 +1,18 @@
+<?php 
+echo $_SERVER["DOCUMENT_ROOT"];
+echo "</br>";
+echo $_SERVER["PHP_SELF"];
+echo "</br>";
+echo $_SERVER["SERVER_NAME"];
+echo "</br>";
+echo $_SERVER["REQUEST_METHOD"];
+echo "</br>";
+// echo $_GET["name"];
+echo "</br>";
+// echo $_REQUEST["name"];
+
+$_SESSION["username"] = "nit";
+echo $_SESSION["username"];
+
+$_ENV[""]
+?>
