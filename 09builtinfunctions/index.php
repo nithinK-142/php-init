a2192a3db96755b12ca57e4693b8d204d36e218b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-18 19:28:11 +0530

    update: builtin math functions.

--- 09builtinfunctions/index.php
+++ 09builtinfunctions/index.php
@@ -12,3 +12,24 @@ echo str_replace("World!","PHP!!",$str);
 
 // explode
 print_r(explode(" ", $str));
+
+
+// math functions
+$number = -7.7;
+// absolute
+echo abs($number);
+
+// round
+echo round($number);
+
+// ceil
+echo ceil($number);
+
+// power
+echo pow(2, 2);
+
+// sqrt
+echo sqrt(64);
+
+// random number
+echo "\n".rand(1,100);
