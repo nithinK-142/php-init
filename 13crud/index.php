500eb1d2b51d141c288d8b17ae0c929bb3e8410a
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-22 16:43:47 +0530

    rename: dbcommunication as crud.

--- 13crud/index.php
+++ 13crud/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>CRUD</title>
     <style>
         body {
             font-family: Arial, sans-serif;
